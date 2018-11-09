db2410c3c33a2a670dcd2da2c3808f9fefdce9dc
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2018-11-09 14:04:19 +0800

    Fix indentation errors.

--- UDPserver.py
+++ UDPserver.py
@@ -40,53 +40,51 @@ if pkt_rcv[2] == "S=1":
         old_seq = ack
         ack = ack+1
         prev_ack =  ack
-    while 1:
-        ready = select.select([serverSocket],[],[],3)
-        if ready[0]:
-            pkt_rcv, clientAddr = pkrecv()
+        while 1:
+            ready = select.select([serverSocket],[],[],3)
+            if ready[0]:
+                pkt_rcv, clientAddr = pkrecv()
 
-            fin = pkt_rcv.split(';')
-            if fin[2]== "F=1":
-                #ack = ack+1
-                fin_ack = int(fin[0].partition('=')[2])+1
-                seq = int(fin[1].partition('=')[2])
-                pkt_snd = "seq="+str(seq)+";ack="+str(fin_ack)+";F=1;len=0"
-                pksend(pkt_snd)
-                #seq = seq+1
-                pkt_snd = "seq="+str(seq)+";ack="+str(fin_ack)+";F=1;len=0"
-                pksend(pkt_snd)
-                fin, clientAddr = pkrecv()
-                fin = fin.split(';')
-                seq = seq+1
-                if int(fin[1].partition('=')[2]) == seq: 
-                    serverSocket.close()
-                    exit()
+                fin = pkt_rcv.split(';')
+                if fin[2]== "F=1":
+                    #ack = ack+1
+                    fin_ack = int(fin[0].partition('=')[2])+1
+                    seq = int(fin[1].partition('=')[2])
+                    pkt_snd = "seq="+str(seq)+";ack="+str(fin_ack)+";F=1;len=0"
+                    pksend(pkt_snd)
+                    #seq = seq+1
+                    pkt_snd = "seq="+str(seq)+";ack="+str(fin_ack)+";F=1;len=0"
+                    pksend(pkt_snd)
+                    fin, clientAddr = pkrecv()
+                    fin = fin.split(';')
+                    seq = seq+1
+                    if int(fin[1].partition('=')[2]) == seq: 
+                        serverSocket.close()
+                        exit()
 
-            elif loss_cnt != 4:
-                print(pkt_rcv)
-                pkt = pkt_rcv.split(';')
-                new_seq = int(pkt[0].partition('=')[2])
-                if prev_ack<new_seq: 
-                    pksend(prev_pkt)                                
-                    continue
-                elif prev_ack>new_seq:
-                    continue
-                ack = ack + int(pkt[3].partition('=')[2])
-                respMsg = "seq="+str(seq)+";ack="+str(ack)+";A=1;len=0"
-                loss_cnt= loss_cnt+1
-                if loss_cnt == 6: #random.randint(5,10):
-                    print("This packet is delayed")
-                    loss_cnt = 0
-                    sleep(5)
-                if old_seq<new_seq:
-                    pksend(respMsg)
-                prev_pkt = respMsg
-                prev_ack = ack
-                old_seq = new_seq
-            else:
-                loss_cnt += 1
-
-
+                elif loss_cnt != 4:
+                    print(pkt_rcv)
+                    pkt = pkt_rcv.split(';')
+                    new_seq = int(pkt[0].partition('=')[2])
+                    if prev_ack<new_seq: 
+                        pksend(prev_pkt)                                
+                        continue
+                    elif prev_ack>new_seq:
+                        continue
+                    ack = ack + int(pkt[3].partition('=')[2])
+                    respMsg = "seq="+str(seq)+";ack="+str(ack)+";A=1;len=0"
+                    loss_cnt= loss_cnt+1
+                    if loss_cnt == 6: #random.randint(5,10):
+                        print("This packet is delayed")
+                        loss_cnt = 0
+                        sleep(5)
+                    if old_seq<new_seq:
+                        pksend(respMsg)
+                    prev_pkt = respMsg
+                    prev_ack = ack
+                    old_seq = new_seq
+                else:
+                    loss_cnt += 1
     else:
         print("Connection establishment failed!!")
 
